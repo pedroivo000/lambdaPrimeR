0620fca9076074906f48afe1a9fe9f7b15363419
Pedro Ivo Guimaraes <pedro_work@bses-iMac.hsd1.va.comcast.net>
2018-03-02 12:38:06 -0500

    Input sequence dataframe display

--- shinylambdaPrimeR/server.R
+++ shinylambdaPrimeR/server.R
@@ -8,11 +8,20 @@ library(lambdaPrimeR)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  # # Reactive object: input sequence type ----
+  # sequence_type <- reactive({
+  #   
+  # })
   # Reactive object: input sequence ---- 
   input_sequence <- eventReactive(input$load_seq_button, {
-    read_sequences(input$input_seq)
+    read_sequences(input$input_seq, input_type = input$sequence_type)
   })
   
-  output$pasted_seq <- renderText({input$input_seq})
+  observeEvent(input$input_seq, {
+    output$sequence_table <- DT::renderDataTable({
+      input_sequence()
+    })
+  })
+  # output$pasted_seq <- renderText({input$input_seq})
 
 })
--- shinylambdaPrimeR/ui.R
+++ shinylambdaPrimeR/ui.R
@@ -10,36 +10,39 @@ shinyUI(navbarPage("lambdaPrimeR", fluid = T,
 
   # Primer design tab -------------------------------------------------------
   tabPanel("Design primers",
-    
     # Sidebar with input and output options ----
     sidebarLayout(
-      
       # Panel in sidebar with sequence inputs ----
       sidebarPanel(
+        # Tabs on sidebar panel ----
+        tabsetPanel(
+          # Input tab on sidebar ----
+          tabPanel("Sequence input",
+            # Input: input sequence type selection ----
+            selectInput('sequence_type', label = 'Sequence type', 
+                        choices = list('Vector' = 'vector', 'Target' = 'target'),
+                        selected = 'vector'),
+            # Input: Paste sequence ----
+            textAreaInput("input_seq", label = "Paste input sequence below:", 
+                          width = '100%'),
+            # Input: Upload file ----
+            fileInput("input_file", "Or upload FASTA file with sequence:", width = '100%'), 
+            # Input: load sequences button ---- 
+            actionButton('load_seq_button', 'Load sequence')
+          ),
         
-        # Sidebar panel title ----
-        titlePanel("Sequence input"),
-        
-        # Input: input sequence type selection ----
-        selectInput('sequence_type', label = 'Sequence type', 
-                    choices = list('Vector' = 'vector', 'Target' = 'target'),
-                    selected = 'vector'),
-        
-        # Input: Paste sequence ----
-        textAreaInput("input_seq", label = "Paste input sequence below:", 
-                      width = '100%'),
-        
-        # Input: Upload file ----
-        fileInput("input_file", "Or upload FASTA file with sequence:", width = '100%'), 
-        
-        # Input: load sequences button ---- 
-        actionButton('load_seq_button', 'Load sequence')
+          # Desing tab on sidebar ----
+          tabPanel("Design",
+            titlePanel("Parameters")
+          )
+        )
       ), 
     
       # Main panel displaying outputs ----
       mainPanel(
         fluidPage(
-        
+          # Output: Sequence info table ----
+          DT::dataTableOutput('sequence_table'),
           # Ouput: Input sequence print ----
           verbatimTextOutput("pasted_seq") 
         )
